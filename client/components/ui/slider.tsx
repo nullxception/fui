d5164f65ddab0ec13dd959a5217d1595f07baec1
Nauval Rizky <enuma.alrizky@gmail.com>
2025-12-17 15:47:11 +0700

    enhance slider component with value badge and improved styling

--- client/components/ui/slider.tsx
+++ client/components/ui/slider.tsx
@@ -1,6 +1,7 @@
 import { cn } from "client/lib/utils";
 import { Slider as SliderPrimitive } from "radix-ui";
 import * as React from "react";
+import { Badge } from "./badge";
 
 function Slider({
   className,
@@ -35,19 +36,25 @@ function Slider({
     >
       <SliderPrimitive.Track
         data-slot="slider-track"
-        className="relative grow overflow-hidden rounded-full bg-muted data-horizontal:h-1 data-horizontal:w-full data-vertical:h-full data-vertical:w-1"
+        className="relative grow overflow-hidden rounded-full bg-muted/80 data-horizontal:h-1.5 data-horizontal:w-full data-vertical:h-full data-vertical:w-1.5"
       >
         <SliderPrimitive.Range
           data-slot="slider-range"
-          className="absolute bg-primary select-none data-horizontal:h-full data-vertical:w-full"
+          className={`absolute ${props.disabled ? "bg-muted" : "bg-primary"} select-none data-horizontal:h-full data-vertical:w-full`}
         />
       </SliderPrimitive.Track>
       {Array.from({ length: _values.length }, (_, index) => (
         <SliderPrimitive.Thumb
           data-slot="slider-thumb"
           key={index}
-          className="relative block size-3 shrink-0 rounded-full border border-ring bg-white ring-ring/50 transition-[color,box-shadow] select-none after:absolute after:-inset-2 hover:ring-[3px] focus-visible:ring-[3px] focus-visible:outline-hidden active:ring-[3px] disabled:pointer-events-none disabled:opacity-50"
-        />
+          className="group block h-3 w-3 rounded-full border border-primary/50 bg-background shadow-sm transition-colors focus-visible:ring-1 focus-visible:ring-ring focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50"
+        >
+          {!props.disabled && (
+            <Badge className="absolute -top-4 left-1/2 -translate-x-1/2 -translate-y-1/2 scale-0 transition-transform group-hover:scale-100">
+              {value}
+            </Badge>
+          )}
+        </SliderPrimitive.Thumb>
       ))}
     </SliderPrimitive.Root>
   );
